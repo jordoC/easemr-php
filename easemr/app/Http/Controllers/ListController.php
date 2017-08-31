d53820eb12d32c48b1cf211c38e7233abfeedbb3
clarke <jordn.clarke@usask.ca>
2017-08-31 16:02:20 +0000

    added default laravel authentication into the application. changed routes so root routes to login page. JC

---
+++ easemr/app/Http/Controllers/ListController.php
@@ -0,0 +1,45 @@
+<?php
+//
+//namespace App\Http\Controllers;
+//
+//use Illuminate\Http\Request;
+//
+//class ListController extends Controller
+//{
+//    //
+//    public function show()
+//    {
+//        $characters = [
+//            'Daenerys Targaryen'    => 'Emilia Clarke',
+//            'Jon Snow'              => 'Kit Harington',
+//            'Arya Stark'            => 'Maisie Williams'
+//        ];
+//
+//        echo $characters
+//
+//        return view('welcome')->withCharacters($characters);
+//    }
+//}
+namespace App\Http\Controllers;
+
+class ListController extends Controller
+{
+    public function show()
+    {
+        $characters = [
+            'Daenerys Targaryen' => 'Emilia Clarke',
+            'Jon Snow'           => 'Kit Harington',
+            'Arya Stark'         => 'Maisie Williams',
+            'Melisandre'         => 'Carice van Houten',
+            'Khal Drogo'         => 'Jason Momoa',
+            'Tyrion Lannister'   => 'Peter Dinklage',
+            'Ramsay Bolton'      => 'Iwan Rheon',
+            'Petyr Baelish'      => 'Aidan Gillen',
+            'Brienne of Tarth'   => 'Gwendoline Christie',
+            'Lord Varys'         => 'Conleth Hill'
+        ];
+
+        return view('welcome')->withCharacters($characters);
+//        return view('welcome')->with(array('characters'=>$characters);
+    }
+}
